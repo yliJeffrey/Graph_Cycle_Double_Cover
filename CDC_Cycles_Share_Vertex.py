bcaf4935eb2d0f52c0e7e40431971df582d56857
yliJeffrey <yuanzhu.li@outlook.com>
2025-08-05 15:14:18 -0400

    CDC_Cycles_Share_Vertex and Main file uploaded

---
+++ CDC_Cycles_Share_Vertex.py
@@ -0,0 +1,41 @@
+# --- CDC Cycles Share Vertex --- #
+# Input: Cycle Double-Cover (CDC) & degree 2 vertex
+# Output: 2 cycles in CDC using the degree 2 vertex
+
+import networkx as nx
+import matplotlib.pyplot as plt
+
+def get_vertex_sharing_cycles(cycle_double_cover, v):
+    vertex_sharing_cycles = []
+    for cycle in cycle_double_cover:
+        for edge in cycle:
+            if v == edge[0] or v == edge[1]:
+                vertex_sharing_cycles.append(cycle)
+                break
+    return vertex_sharing_cycles
+
+
+# Testing
+G = nx.Graph()
+G.add_edge(0,1)
+G.add_edge(0,2)
+G.add_edge(0,3)
+G.add_edge(1,3)
+G.add_edge(2,3)
+
+
+cycle_double_cover = [[(0,2),(2,3),(3,0)], [(0,1),(1,3),(3,0)], [(0,1),(1,3),(3,2),(2,0)]]
+print(f"The Cycle Double-Cover:")
+for cycle in cycle_double_cover:
+    print(cycle)
+print("\n")
+
+vertex_sharing_cycles = get_vertex_sharing_cycles(cycle_double_cover, 1)
+print("Vertex Sharing Cycles:")
+for cycle in vertex_sharing_cycles:
+    print(cycle)
+
+pos_G = nx.spring_layout(G, seed=42)
+nx.draw(G, pos=pos_G, with_labels=True)
+plt.show()
+
---
+++ Main.py
@@ -0,0 +1,20 @@
+# Find the Cycle Double-Cover Given a Bridgeless Subcubic Graph
+# Input: bridgeless subcubic graph G
+# Output: cycle double-cover
+
+'''
+Step 1: Ear Decomposition
+Step 2: Form Intersection Graph
+Step 3: Find Induced Path between two vertices
+Step 4: 
+Step 5:
+Step 6:
+'''
+
+
+def main():
+    print()
+
+
+if __name__ == "__main__":
+    main()
